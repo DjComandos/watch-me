ce66c6c688031f0dc5109d7f54875c960318e78a
Mikita Manko <DjComandos@gmail.com>
2012-11-24 23:11:04 +0300

    merged with web cam broadcating code

--- server.js
+++ server.js
@@ -7,16 +7,21 @@ var path = require('path'),
     app = express(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server),
-    users = {};     /* contains all users connected to the server */
+    gameState = {}, /* contains TicTacToe object for each created game */
+    users = {},     /* contains all users connected to the server */
+    watchers = {}, /* contains array of watchers' sockets for each flow */
+    games = {};     /* contains all created games */
 
 app.use(express.static(path.join(__dirname, 'static')));
 io.set('log level', 1);
 
 io.sockets.on('connection', function (socket) {
-    var userName;
+    var userName,
+        currentGame,
+        isMaster = false;
     
     function changeUsersCount(){
-        socket.broadcast.json.emit('usersListChanged', {'users': users});
+        socket.broadcast.json.emit('usersListChanged', {'users': users, 'games': games});
     }
     
     socket.json.emit({'event': 'connected', 'time': (new Date).toLocaleTimeString()});
@@ -26,7 +31,7 @@ io.sockets.on('connection', function (socket) {
         if(!!data && !!data.name && !users[data.name.toLowerCase()]) {
             userName = data.name;
             users[userName.toLowerCase()] = userName;
-            socket.json.emit('userloggedin', {'name': userName, 'users': users});
+            socket.json.emit('userloggedin', {'name': userName, 'users': users, 'games': games});
             changeUsersCount();
         } else if (users[data.name.toLowerCase()]) {
             socket.json.emit('onLogInError', {'msg': 'user with name ' + data.name + ' has already connected'});
@@ -36,10 +41,62 @@ io.sockets.on('connection', function (socket) {
     });
     
 
+    socket.on('createGame', function (data){
+        currentGame = ([userName, '_', data.gameName]).join('');
+        isMaster = true;
+        games[currentGame] = { id: currentGame, 
+                                name: data.gameName,
+                                user: userName, 
+                                isAvailable: true
+                            };
+        
+        watchers[currentGame] = {ownersSocket: socket, watchersSockets: []};
+
+        //console.log('streamsServerVideo' + currentGame + ' subscribed');
+
+        socket.on('streamsServerVideo' + currentGame, function (clientRequest, sendToClientCallback) {
+            //console.log('streamsServerVideo' + currentGame + ' pachet received');
+            if(watchers[currentGame].watchersSockets.length > 0) {
+                io.sockets.volatile.emit('streamsServerVideo' + currentGame, clientRequest.base64Frame );
+            }
+        });
+
+        socket.json.emit('gameHosted', {'game': games[currentGame]});
+        socket.broadcast.json.emit('gamesListChanged', {'games': games});
+    });
+    
+
+    socket.on('connectToGame', function (data){
+        currentGame = data.gameName;
+        if(!!games[currentGame]) {
+            var isFirstWatcher = watchers[currentGame].watchersSockets.length == 0;
+            watchers[currentGame].watchersSockets.push(socket);
+
+            var gameData = { name: currentGame, 
+                        user: games[currentGame].user
+                    };
+
+            socket.json.emit('gameStarted', gameData);
+            if(isFirstWatcher) {
+                gameData.isMaster = true;
+                watchers[currentGame].ownersSocket.json.emit('gameStarted', gameData);
+            }
+
+        } else {
+            console.log('Game with name "' + currentGame + '" not found');
+        }
+
+    });
+    
+
     socket.on('disconnect', function() {
         isMaster = false;
         if(!!userName) {
             delete users[userName.toLowerCase()];
+        }
+        if(!!currentGame) {
+            delete games[currentGame]; 
+            delete gameState[currentGame];
         }
         changeUsersCount();
     });
